f2851c1f231960d9b9db16aab513ce9f64ab1817
student <brbrunda.sai@hcl.com>
2021-04-21 07:55:00 +0000

    my commit

---
+++ src/main/java/com/brunda/springboot/DevOpsGraded/DevOpsGradedApplication.java
@@ -0,0 +1,14 @@
+package com.brunda.springboot.DevOpsGraded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsGradedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsGradedApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
---
+++ src/test/java/com/brunda/springboot/DevOpsGraded/DevOpsGradedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunda.springboot.DevOpsGraded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsGradedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
